0069f87e859e0e621756c6d5050b08d34765c6f6
Amy <acachero01@gmail.com>
2026-08-10 17:46:45 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Shows from "./pages/Shows";
+import Music from "./pages/Music";
+import MeetTheRats from "./pages/Meet-The-Rats";
+import Fun from "./pages/Fun"
+import styles_global from "./styles/App.module.css";
+
+import { AnimatePresence } from "framer-motion";
+
+function AnimatedRoutes() {
+  const location = useLocation()
+  return (
+    <AnimatePresence mode="wait" initial={false}>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/shows" element={<Shows />} />
+          <Route path="/music" element={<Music />} />
+          <Route path="/meet-the-rats" element={<MeetTheRats />} />
+          <Route path="/fun" element={<Fun />} />
+        </Routes>
+      </AnimatePresence>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles_global.app_container}>
+      <BrowserRouter >
+      <Header></Header>
+      <AnimatedRoutes></AnimatedRoutes>
+    </BrowserRouter>
     </div>
   );
 }
